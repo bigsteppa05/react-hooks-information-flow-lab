6b558839906eafe8fd582042538f8c7477ae5a0b
bigsteppa05 <habibforderiv05@gmail.com>
2024-04-28 12:00:24 +0300

    done

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import ShoppingList from "./ShoppingList";
+import Header from "./Header";
+import itemData from "../data/items";
+
+function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  function onDarkModeClick() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
+
+  return (
+    <div className={"App " + (isDarkMode ? "dark" : "light")}>
+      <Header onDarkModeClick={onDarkModeClick} />
+      <ShoppingList items={itemData} />
+    </div>
+  );
+  }
+  
+
+export default App;
